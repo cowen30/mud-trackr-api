70c366f06adf4df2aff8d51e6a97ee33b8bfa5a6
Zach Cowen <cowen.30@osu.edu>
2022-08-21 16:22:07 -0400

    fixing foreign key refs in migration scripts

--- db/migrate/20220612160002_create_event_details.rb
+++ db/migrate/20220612160002_create_event_details.rb
@@ -2,8 +2,8 @@ class CreateEventDetails < ActiveRecord::Migration[7.0]
 
 	def change
 		create_table :event_details do |t|
-			t.references :events, foreign_key: true
-			t.references :event_types, foreign_key: true
+			t.references :event, foreign_key: true
+			t.references :event_type, foreign_key: true
 			t.date :start_date
 			t.date :end_date
 			t.decimal :lap_distance
--- db/migrate/20220612160004_create_age_groups.rb
+++ db/migrate/20220612160004_create_age_groups.rb
@@ -2,7 +2,7 @@ class CreateAgeGroups < ActiveRecord::Migration[7.0]
 
 	def change
 		create_table :age_groups do |t|
-			t.references :event_details, foreign_key: true
+			t.references :event_detail, foreign_key: true
 			t.string :name
 			t.string :gender
 			t.integer :min_age
