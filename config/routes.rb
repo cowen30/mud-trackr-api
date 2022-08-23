7efa7e365bfe2b45e9bf8d35d226dbf5755ad13e
Zach Cowen <cowen.30@osu.edu>
2022-08-23 18:22:23 -0400

    cleanup routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
 	# Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-	# Defines the root path route ("/")
-	# root "articles#index"
 	resources :results
-	# resources :events, only: [:index, :show] do
-	# 	resources :results
-	# end
 end
