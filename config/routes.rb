c60c13c6612b1b314315606e2d855c2c6ac9d6fb
Zach Cowen <cowen.30@osu.edu>
2022-09-10 14:25:01 -0400

    add health check endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 	# Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+	get '/health-check', to: proc { [200, {}, ['OK']] }
+
 	resources :events
 	resources :event_details, path: '/event-details'
 	resources :results
